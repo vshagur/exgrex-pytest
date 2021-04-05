e9eaab8d29289b9a3bcf3ca0ef8b51d6f353567e
Valeriy Shagur <vshagur@gmail.com>
2021-04-05 11:29:43 +0700

    [release] up version to 0.1a2

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("README", "r") as fh:
     long_description = fh.read()
 
 setup(name='exgrex-pytest',
-      version='0.1a1',
+      version='0.1a2',
       description='Package exgrex-pytest adds to package exgrex-py the ability to use '
                   'the pytest framework.',
       long_description=long_description,
